6f64d2ee02c70bf6d9d79930db6e8753fd901bcb
Hunter Browning <guccigod@Hunters-MBP.lan>
2022-03-21 15:39:42 -0400

    Added Password Hashing

--- ElevenNote.WebAPI/Controllers/UserController.cs
+++ ElevenNote.WebAPI/Controllers/UserController.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using ElevenNote.Models.User;
 using ElevenNote.Services.User;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
@@ -17,5 +18,22 @@ namespace ElevenNote.WebAPI.Controllers
         {
             _service = service;
         }
+
+        [HttpPost("Register")]  // Register user
+        public async Task<IActionResult> RegisterUser([FromBody] UserRegister model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var registerResult = await _service.RegisterUserAsync(model);
+            if (registerResult)
+            {
+                return Ok("User was registered.");
+            }
+            return BadRequest("User could not be registered.");
+        }
     }
+
 }
